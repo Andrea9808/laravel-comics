23b3650f824502f2581c84c72139ea6c11e50a9e
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-02-09 17:53:05 +0100

    feat: footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,67 @@
 <footer>
-    <h1>Hello from footer</h1>
+
+    <div class="container-fluid gx-0">
+        <div class="bg_footer">
+
+            <div class="container">
+                <div class="navbar_footer">
+
+                    <div class="link">
+                        <h5>DC COMICS</h5>
+                        <ul>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+                        <h5>SHOP</h5>
+                        <ul>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collections</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="link">
+                        <h5>DC</h5>
+                        <ul>
+                            <li><a href="#">Terms of Use</a></li>
+                            <li><a href="#">Privacy Policy (New)</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshops</a></li>
+                            <li><a href="#">CPSC Certificates</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+
+
+                    <div class="link">
+                        <h5>SITES</h5>
+                        <ul>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+
+                <div class="logo_bg">
+
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
 </footer>
--- resources/views/components/shop.blade.php
+++ resources/views/components/shop.blade.php
@@ -1,1 +1,17 @@
-<h1 >hfdhgcfhg</h1>
+
+<div class="container-fluid gx-0">
+    <div class="bg_shop">
+        <div class="container">
+            <div class="row align-items-center justify-content-center">
+                @foreach ($shops as $shop )
+                    <div class="col-2">
+                        <div class="card_shop">
+                            <img src="{{ Vite::asset($shop['img']) }}" alt="img-shop">
+                            <span class="text-white">{{$shop['text']}}</span>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
