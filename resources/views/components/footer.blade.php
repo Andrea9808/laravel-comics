cc9c68583ed22a8a6c4554304c3c02d99ca6fd80
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-02-09 18:30:42 +0100

    fix: footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,54 +5,62 @@
 
             <div class="container">
                 <div class="navbar_footer">
+                    <div class="navbar_links">
 
-                    <div class="link">
-                        <h5>DC COMICS</h5>
-                        <ul>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Comics</a></li>
-                            <li><a href="#">Movies</a></li>
-                            <li><a href="#">TV</a></li>
-                            <li><a href="#">Games</a></li>
-                            <li><a href="#">Videos</a></li>
-                            <li><a href="#">News</a></li>
-                        </ul>
-                        <h5>SHOP</h5>
-                        <ul>
-                            <li><a href="#">Shop DC</a></li>
-                            <li><a href="#">Shop DC Collections</a></li>
-                        </ul>
+
+                        <div class="link">
+                            <h5>DC COMICS</h5>
+                            <ul>
+                                <li><a href="#">Characters</a></li>
+                                <li><a href="#">Comics</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">TV</a></li>
+                                <li><a href="#">Games</a></li>
+                                <li><a href="#">Videos</a></li>
+                                <li><a href="#">News</a></li>
+                            </ul>
+                            <h5>SHOP</h5>
+                            <ul>
+                                <li><a href="#">Shop DC</a></li>
+                                <li><a href="#">Shop DC Collections</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="link">
+                            <h5>DC</h5>
+                            <ul>
+                                <li><a href="#">Terms of Use</a></li>
+                                <li><a href="#">Privacy Policy (New)</a></li>
+                                <li><a href="#">Ad Choices</a></li>
+                                <li><a href="#">Advertising</a></li>
+                                <li><a href="#">Jobs</a></li>
+                                <li><a href="#">Subscriptions</a></li>
+                                <li><a href="#">Talent Workshops</a></li>
+                                <li><a href="#">CPSC Certificates</a></li>
+                                <li><a href="#">Ratings</a></li>
+                                <li><a href="#">Shop Help</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                            </ul>
+                        </div>
+
+
+                        <div class="link">
+                            <h5>SITES</h5>
+                            <ul>
+                                <li><a href="#">DC</a></li>
+                                <li><a href="#">MAD Magazine</a></li>
+                                <li><a href="#">DC Kids</a></li>
+                                <li><a href="#">DC Universe</a></li>
+                                <li><a href="#">DC Power Visa</a></li>
+                            </ul>
+                        </div>
                     </div>
 
-                    <div class="link">
-                        <h5>DC</h5>
-                        <ul>
-                            <li><a href="#">Terms of Use</a></li>
-                            <li><a href="#">Privacy Policy (New)</a></li>
-                            <li><a href="#">Ad Choices</a></li>
-                            <li><a href="#">Advertising</a></li>
-                            <li><a href="#">Jobs</a></li>
-                            <li><a href="#">Subscriptions</a></li>
-                            <li><a href="#">Talent Workshops</a></li>
-                            <li><a href="#">CPSC Certificates</a></li>
-                            <li><a href="#">Ratings</a></li>
-                            <li><a href="#">Shop Help</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                        </ul>
-                    </div>
 
-
-                    <div class="link">
-                        <h5>SITES</h5>
-                        <ul>
-                            <li><a href="#">DC</a></li>
-                            <li><a href="#">MAD Magazine</a></li>
-                            <li><a href="#">DC Kids</a></li>
-                            <li><a href="#">DC Universe</a></li>
-                            <li><a href="#">DC Power Visa</a></li>
-                        </ul>
-                    </div>
-
+                    <p>All Site Content TM and © 2020 DC Entertainment, unless otherwise <span
+                        class="text-primary">noted here</span>. All rights reserved. <span
+                        class="text-primary">Cookies Settings</span>
+                    </p>
 
                 </div>
 
@@ -62,9 +70,7 @@
                 </div>
 
             </div>
-            <div class="container">
-                <p>All Site Content TM and © 2020 DC Entertainment, unless otherwise <span class="text-primary">noted here</span>. All rights reserved. <span class="text-primary">Cookies Settings</span></p>
-            </div>
+
         </div>
 
     </div>
