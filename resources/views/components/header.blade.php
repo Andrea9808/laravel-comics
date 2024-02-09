2014fc9c371f1176504aa80fd85ca4487c3cb6e5
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-02-09 13:58:55 +0100

    config: header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,29 @@
 <header>
-    <h1>Hello from Header</h1>
+
+    <div class="container-fluid gx-0">
+        <div class="bg">
+            <div class="container d-flex justify-content-end">
+                <span class="text-white px-5">DC POWER VISA</span>
+                <span class="text-white">ADDITIONAL DC SITES</span>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="d-flex align-items-center justify-content-between">
+                <div class="logo">
+                    <img src="{{ Vite::asset('resources/imgs/dc-logo.png') }}">
+                </div>
+
+                <div class="text_header">
+                    {{-- character. comics,movies ecc.. --}}
+                </div>
+
+                <div class="search">
+                    <input type="text" placeholder="Search">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </header>
